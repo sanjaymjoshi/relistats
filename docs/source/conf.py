5ce9cfd4281654d26c789c3511dab84d4f680df8
sanjaymjoshi <sanjaymjoshi@gmail.com>
2023-02-27 07:04:17 -0500

    conf.py format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,32 +6,32 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import sys
 import os
-sys.path.insert(0, os.path.abspath('.'))
+import sys
 
-project = 'Reliability Statistics'
-copyright = '2023, Sanjay M Joshi'
-author = 'Sanjay M Joshi'
+sys.path.insert(0, os.path.abspath("."))
+
+project = "Reliability Statistics"
+copyright = "2023, Sanjay M Joshi"
+author = "Sanjay M Joshi"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]

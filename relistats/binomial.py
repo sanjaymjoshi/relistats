ebdc4e298aaadbd013fb950befac60c7b9746e24
sanjaymjoshi <sanjaymjoshi@gmail.com>
2023-02-27 20:37:12 -0500

    sphnix docs

--- relistats/binomial.py
+++ relistats/binomial.py
@@ -6,11 +6,16 @@ import scipy.stats as st
 
 
 def confidence(n: int, f: int, r: float) -> Optional[float]:
-    """Returns confidence [0, 1] in reliability r
-    n -- number of samples, >= 0
-    f -- number of failures >= 0
-    r -- reliability level [0, 1]
-    Returns None if confidence could not be computed
+    """Confidence [0, 1] in reliability r using closed-form expression.
+
+    :param n: number of samples
+    :type n: int, >=0
+    :param f: number of failures
+    :type f: int, >=0
+    :param r: reliability level
+    :type r: float, [0, 1]
+    :return: Confidence or None if it could not be computed
+    :rtype: float, optional
     """
     if n <= 0 or f < 0 or r < 0 or r > 1:
         return None
@@ -19,20 +24,14 @@ def confidence(n: int, f: int, r: float) -> Optional[float]:
     return st.binom.sf(f, n, prob_failure)
 
 
-# Wilson score interval from
-# https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval
-# Wallis, Sean A. (2013). "Binomial confidence intervals and contingency tests: mathematical fundamentals and the evaluation of alternative methods".
-# Journal of Quantitative Linguistics. 20 (3): 178–208.
-
-
 def _wilson_center(p, n, c):
-    """Center of Wilson score interval. See comment above."""
+    """Center of Wilson score interval. See reference below."""
     z = st.norm.ppf(c)
     return (p + z * z / (2 * n)) / (1 + z * z / n)
 
 
 def _wilson_lower(p, n, c):
-    """Lower bound of Wilson score interval. See comment above."""
+    """Lower bound of Wilson score interval. See reference below."""
     z = st.norm.ppf(c)
     p50 = _wilson_center(p, n, c)
     part2 = z / (1 + z * z / n) * sqrt(p * (1 - p) / n + z * z / (4 * n * n))
@@ -45,13 +44,22 @@ def _wilson_lower_corrected(p, n, c):
 
 
 def reliability_closed(n: int, f: int, c: float) -> Optional[float]:
-    """Returns approximate minimum reliability [0, 1] at confidence level c
+    """Approximate minimum reliability [0, 1] at confidence level c.
     The approximation is within about 5% of actual reliability and uses
-    closed-form expression for computation
-    n -- number of samples, >= 0
-    f -- number of failures >= 0
-    c -- confidence level [0, 1]
-    Returns None if reliability could not be computed
+    closed-form expression for computation called 'Wilson Score Interval
+    with Continuity Correction' [Wallis, Sean A. (2013). "Binomial
+    confidence intervals and contingency tests: mathematical fundamentals
+    and the evaluation of alternative methods". Journal of Quantitative
+    Linguistics. 20 (3): 178–208].
+
+    :param n: number of samples
+    :type n: int, >=0
+    :param f: number of failures
+    :type f: int, >=0
+    :param c: confidence level
+    :type c: float, [0, 1]
+    :return: Reliability or None if it could not be computed
+    :rtype: float, optional
     """
     if n <= 0 or f < 0 or c < 0 or c > 1:
         return None
@@ -66,13 +74,21 @@ def _reliability_fn(x: float, n: int, f: int, c: float) -> float:
 
 
 def reliability_optim(n: int, f: int, c: float, tol=0.001) -> Optional[float]:
-    """Returns minimum reliability [0, 1] at confidence level c using numerical optimization
-    The approximation is within about specified tolerance limit
-    n -- number of samples, >= 0
-    f -- number of failures >= 0
-    c -- confidence level [0, 1]
-    tol -- [optional] accurancy tolerance limit]
-    Returns None if reliability could not be computed
+    """Minimum reliability [0, 1] at confidence level c using numerical
+    optimization (Brent's method). The approximation is within specified
+    tolerance limit.
+
+    :param n: number of samples
+    :type n: int, >=0
+    :param f: number of failures
+    :type f: int, >=0
+    :param c: confidence level
+    :type c: float, [0, 1]
+    :param tol: accuracy tolerance
+    :type tol: float, optional
+
+    :return: Reliability or None if it could not be computed
+    :rtype: float, optional
     """
     if n <= 0 or f < 0 or c < 0 or c > 1:
         return None
@@ -90,10 +106,15 @@ def reliability_optim(n: int, f: int, c: float, tol=0.001) -> Optional[float]:
 
 def reliability(n: int, f: int, c: float) -> Optional[float]:
     """Minimum reliability at confidence level c
-    n -- number of samples, >= 0
-    f -- number of failures >= 0
-    c -- confidence level [0, 1]
-    Returns None if reliability could not be computed
+
+    :param n: number of samples
+    :type n: int, >=0
+    :param f: number of failures
+    :type f: int, >=0
+    :param c: confidence level
+    :type c: float, [0, 1]
+    :return: Reliability or None if it could not be computed
+    :rtype: float, optional
     """
     return reliability_optim(n, f, c)
 
@@ -105,11 +126,19 @@ def _assurance_fn(x: float, n: int, f: int) -> float:
 
 
 def assurance(n: int, f: int, tol=0.001) -> Optional[float]:
-    """Returns assurance [0, 1], i.e., confidence = reliability
-    n -- number of samples, >= 0
-    f -- number of failures >= 0
-    tol -- [optional] accurancy tolerance limit
-    Returns None if assurance could not be computed
+    """Assurance [0, 1], i.e., confidence = reliability. For example,
+    90% assurance means 90% confidence in 90% reliability (at n=22, f=0).
+    This method uses numerical approach of Brent's method to compute
+    the solution within the specified tolerance.
+
+    :param n: number of samples
+    :type n: int, >=0
+    :param f: number of failures
+    :type f: int, >=0
+    :param tol: accuracy tolerance
+    :type tol: float, optional
+    :return: Assurance or None if it could not be computed
+    :rtype: float, optional
     """
     if n <= 0 or f < 0:
         return None

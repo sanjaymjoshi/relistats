1e1d6232491ed78956f9adb82872acd68b78c780
sanjaymjoshi <sanjaymjoshi@gmail.com>
2023-04-17 22:07:19 -0400

    unused code

--- test/test_binom_fin.py
+++ test/test_binom_fin.py
@@ -1,43 +1,13 @@
-from dataclasses import asdict, dataclass
-
 import pytest
 
 from relistats.binom_fin import (
-    #assurance,
     conf_fin,
     reli_fin,
     assur_fin
 )
 
-
-@dataclass
-class NFRC:
-    n: int
-    f: int
-    r: float
-    c: float
-
-
-test_nfrc = (
-    NFRC(1, 0, 0.5, 0.5),
-    NFRC(2, 0, 0.5, 0.75),
-    NFRC(2, 1, 0.5, 0.25),
-    NFRC(8, 0, 0.7, 0.942),
-    NFRC(8, 0, 0.9, 0.570),
-    NFRC(10, 9, 0.1, 0.349),
-    NFRC(100, 10, 0.9, 0.417),
-    NFRC(1000, 100, 0.9, 0.473),
-    NFRC(10000, 1000, 0.9, 0.492),
-)
-
-
 def test_conf_fin() -> None:
-
-    # Confidence computation is exact, so set the tolerance tight
     ABS_TOL_CONFIDENCE = 0.001
-#    for x in test_nfrc:
-#        print(f"Testing confidence: {asdict(x)}")
-#        assert confidence(x.n, x.f, x.r) == pytest.approx(x.c, abs=ABS_TOL_CONFIDENCE)
 
     assert conf_fin(4, 0, 0.5, 0) == (1, 0.5)
     assert conf_fin(4, 1, 0.5, 0) == (1, 0.5)
@@ -59,14 +29,7 @@ def test_conf_fin() -> None:
 
 
 def test_reli_fin() -> None:
-#     # Reliability computation should be accurate, so set the tolerance tight
     ABS_TOL_RELIABILITY = 0.001
-#     for x in test_nfrc:
-#         print(f"Testing reliability: {asdict(x)}")
-#         c1 = confidence(x.n, x.f, x.r)
-#         if c1 is None:
-#             c1 = 0
-#         assert reliability(x.n, x.f, c1) == pytest.approx(x.r, abs=ABS_TOL_RELIABILITY)
     assert reli_fin(4, 0, 0.5, 0) == (1, 1)
     assert reli_fin(4, 1, 0.5, 0) == (0.75, 1)
     assert reli_fin(4, 2, 0.5, 0) == (0.5, 1)

53bd7a0d78638e303350e206bc06c93d4d550005
sanjaymjoshi <sanjaymjoshi@gmail.com>
2024-01-17 20:31:22 -0500

    refactor intervals

---
+++ relistats/intervals.py
@@ -0,0 +1,264 @@
+"""Statistical methods for confidence and tolerance intervals
+"""
+from typing import Any, Optional
+
+import scipy.optimize as opt
+
+from relistats import logger
+from relistats.percentile import (
+    _confidence_invalid,
+    _num_samples_invalid,
+    _percentile_invalid,
+    confidence_in_percentile,
+)
+
+
+def _percentile_interval_candidates(
+    n: int, p: float, c: float
+) -> list[tuple[int, int]]:
+    """Returns list of tuples of two indices, out of n, that meet requirement that
+       percentile/quantile p, 0 < p < 1 lies within these two indices with
+       confidence >= c, 0 < c < 1.
+
+    Returns empty list if none found.
+    """
+    c_max = confidence_in_percentile(n, n, p)
+    c_min = confidence_in_percentile(1, n, p)
+    if c_max - c_min < c:
+        logger.info(
+            "Highest confidence %f < required %f, n=%d, pp=%f", c_max - c_min, c, n, p
+        )
+        return []
+
+    # Start from mid-point and expand until first candidate for higher bound
+    j_hi = (n + 1) // 2
+    j_lo = 1
+    c_lo = c_min
+    while j_hi <= n:
+        c_hi = confidence_in_percentile(j_hi, n, p)
+        if c_hi - c_lo > c:
+            logger.debug(
+                "First candidate found, j_hi=%d, c_hi=%f - c_lo=%f > c=%f",
+                j_hi,
+                c_hi,
+                c_lo,
+                c,
+            )
+            break
+        j_hi += 1
+
+    rc: list[tuple[int, int]] = []
+    while j_hi <= n:
+        c_hi = confidence_in_percentile(j_hi, n, p)
+        c_lo_temp = confidence_in_percentile(j_lo, n, p)
+        while c_hi - c_lo_temp > c:
+            j_lo += 1
+            c_lo_temp = confidence_in_percentile(j_lo, n, p)
+        # Now step back j_lo
+        j_lo -= 1
+        logger.debug(
+            "List candidate found, j_lo=%d, c_hi=%f - c_lo=%f > c=%f",
+            j_lo,
+            c_hi,
+            c_lo_temp,
+            c,
+        )
+        rc.append((j_lo, j_hi))
+        j_hi += 1
+
+    return rc
+
+
+def percentile_interval_places(n: int, p: float, c: float) -> Optional[tuple[int, int]]:
+    """Returns tuple of two places (1..n) such that percentile/quantile p
+    (0 < p < 1) lies within these two places of n sorted samples with confidence
+    of at least c (0 < c < 1).
+
+    Note that the places are not indexed at zero!
+
+    Use this method if you plan to sort samples yourself, else you can use
+    confidence_interval_of_quantile method.
+
+    Return None if such a tuple cannot be computed. If that happens, try to increase n,
+    reduce pp, or reduce c.
+    """
+    if _percentile_invalid(p) or _confidence_invalid(c) or _num_samples_invalid(n):
+        return None
+
+    candidates = _percentile_interval_candidates(n, p, c)
+    if len(candidates) == 0:
+        return None
+    interval_sizes = [x[1] - x[0] for x in candidates]
+    min_id = interval_sizes.index(min(interval_sizes))
+    return candidates[min_id]
+
+
+def tolerance_interval_places(n: int, t: float, c: float) -> Optional[tuple[int, int]]:
+    """Returns tolerance interval places. Out of n sorted samples, a fraction of t samples
+    (0 < t < 1) are expected to be within these two places, with a probability of at least c,
+    0 < c < 1.
+
+    Returns None if such tuple cannot be calculated. If that happens, try to increase n,
+    reduce t, or reduce c.
+    """
+    if _percentile_invalid(t) or _confidence_invalid(c) or _num_samples_invalid(n):
+        return None
+
+    # Construct the interval (g, h) in two halves, each with t/2 fraction around median.
+    # Let c_hi = probability that h out of n samples are smaller than quantile (0.5+t/2)
+    # Let c_lo = probability that g out of n samples are smaller than quantile (0.5-t/2)
+    # 1 - c_lo = probability that g out of n samples are higher than quantile (0.5-t/2)
+    # Then c_hi*(1 - c_lo) = probability that between (g, h) samples out of n are
+    # within quantile t
+
+    # At the high end, start at the median and expand until confidence exceeds c for
+    # quantile of 0.5 + t/2.
+    median_index = (n + 1) // 2
+    j_hi = median_index
+    p_hi = 0.5 + t / 2
+    while confidence_in_percentile(j_hi, n, p_hi) < c:
+        j_hi += 1
+        if j_hi == n + 1:
+            logger.error(
+                "Not enough samples, %d, for tolerance %f at confidence %f.",
+                n,
+                t,
+                c,
+            )
+            return None
+    c_hi = confidence_in_percentile(j_hi, n, p_hi)
+    logger.debug(
+        "p_hi = %f, j_hi = %d, c_hi = %f, c_lo < %f", p_hi, j_hi, c_hi, 1 - c / c_hi
+    )
+
+    # Now start at zero index and quantile of 0.5 - t/2
+    # c_hi * (1 - c_lo) >= c
+    # c_lo <=  1 - c / c_hi
+    j_lo = 0
+    p_lo = 0.5 - t / 2
+    logger.debug(
+        "p_lo = %f, j_lo = %d, c_lo = %f",
+        p_lo,
+        j_lo,
+        confidence_in_percentile(j_lo, n, p_lo),
+    )
+    while confidence_in_percentile(j_lo, n, p_lo) < 1 - c / c_hi:
+        logger.debug(
+            "p_lo = %f, j_lo = %d, c_lo = %f",
+            p_lo,
+            j_lo,
+            confidence_in_percentile(j_lo, n, p_lo),
+        )
+        j_lo += 1
+        if j_lo == median_index:
+            logger.error(
+                "Not enough samples, %d, for tolerance %f at confidence %f. Need more.",
+                n,
+                t,
+                c,
+            )
+            return None
+
+    return (j_lo + 1, j_hi)
+
+
+def assurance_interval_places(n: int, a: float) -> Optional[tuple[int, int]]:
+    """Returns assurance interval places. Out of n sorted samples, a fraction of a samples
+    are expected to be within these two places, with a probability of at least a.
+
+    Returns None if such tuple cannot be calculated. If that happens, try to increase n
+    or reduce a.
+    """
+    return tolerance_interval_places(n, a, a)
+
+
+def confidence_interval_of_median(c: float, *args) -> Optional[tuple[Any, Any]]:
+    """Returns median interval from args at confidence of at least c, if possible.
+    Returns None if not possible.
+    args is any iterable (list, tuple, set)
+    """
+    return confidence_interval_of_percentile(0.5, c, *args)
+
+
+def confidence_interval_of_percentile(
+    p: float, c: float, *args
+) -> Optional[tuple[Any, Any]]:
+    """Returns p'th percentile/quantile interval from args at confidence of at least c, if possible.
+    Use this method if you data is not sorted already, else you can use quantile_interval_places.
+    Returns None if not possible.
+    args is any iterable (list, tuple, set)
+    """
+    n = len(*args)
+    ii = percentile_interval_places(n, p, c)
+    # Need to subtract 1 from the places, to account for 0-based index
+    return (
+        tuple(sorted(*args)[slice(ii[0] - 1, ii[1], ii[1] - ii[0])]) if ii else None  # type: ignore
+    )
+
+
+def tolerance_interval(t: float, c: float, *args) -> Optional[tuple[Any, Any]]:
+    """Returns tolerance interval for middle t (0<t<1) fraction of samples,
+    with confidence c (0<c<1), if possible.
+    Use this method if you data is not sorted already, else you can use tolerance_interval_places.
+    Returns None if not possible.
+    args is any iterable (list, tuple, set)
+    """
+    n = len(*args)
+    ii = tolerance_interval_places(n, t, c)
+    # Need to subtract 1 from the places, to account for 0-based index
+    return (
+        tuple(sorted(*args)[slice(ii[0] - 1, ii[1], ii[1] - ii[0])]) if ii else None  # type: ignore
+    )
+
+
+def _assurance_interval_fn(x: float, j_lo: int, j_hi: int, n: int) -> float:
+    """Function to find roots of x = confidence_in_quantile(n, f, x)"""
+    x_hi_hat = confidence_in_percentile(j_hi, n, x) or 0
+    x_lo_hat = confidence_in_percentile(j_lo, n, 1 - x) or 0
+    x_hat = min(x_hi_hat, 1 - x_lo_hat)
+    return x_hat - x
+
+
+def assurance_in_interval(j_lo: int, j_hi: int, n: int, tol=0.001) -> Optional[float]:
+    """Assurance level for interval [j_lo, j_hi] out of n sorted samples. Assurance
+    level of a means a% of samples will be within this interval with a% confidence.
+    Example: Out of 16 ordered samples, we can be 80% confident that 80% samples will
+    be between 1st and 15th place.
+
+    :param j_lo: sample place at lower end
+    :type j_lo: int, >0
+    :param j_hi: sample place at upper end
+    :type j_hi: int, n > j_hi > j_lo
+    :param n: number of samples
+    :type n: int, >=0
+    :param tol: accuracy tolerance
+    :type tol: float, optional
+
+    :return: Assurance or None if it could not be computed
+    :rtype: float, optional
+    """
+    if _num_samples_invalid(n):
+        return None
+
+    if j_lo <= 0 or j_lo > n - 1 or j_hi <= 0 or j_hi > n - 1:
+        logger.error(
+            "Sample places %d, %d out of range, need to be between 0 and %d",
+            j_lo,
+            j_hi,
+            n - 1,
+        )
+        return None
+
+    if j_lo >= j_hi:
+        logger.error("Places %d >= %d not supported", j_lo, j_hi)
+        return None
+
+    # Use numerical optimization to find real root of the confidence equation
+    # x - _assurance_interval_fn(x, j_lo, j_hi, n)
+    return opt.brentq(
+        _assurance_interval_fn,
+        a=0,  # Lowest possible value
+        b=1,  # Highest possible value
+        args=(j_lo, j_hi, n),
+        xtol=tol,
+    )
--- test/test_percentile.py
+++ test/test_percentile.py
@@ -1,15 +1,6 @@
 import pytest
 
-from relistats.percentile import (
-    assurance_in_interval,
-    assurance_in_percentile,
-    confidence_in_percentile,
-    confidence_interval_of_median,
-    confidence_interval_of_percentile,
-    percentile_interval_places,
-    tolerance_interval,
-    tolerance_interval_places,
-)
+from relistats.percentile import assurance_in_percentile, confidence_in_percentile
 
 
 def confidence_from_textbook_example(n, i, j, p) -> float:
@@ -79,99 +70,5 @@ def test_confidence_in_quantile_at_index() -> None:
     assert confidence_in_percentile(1, 20, 0.05) == pytest.approx(0.358, abs=0.01)
 
 
-def test_confidence_interval_indices_in_quantile() -> None:
-    # Same numbers from the table in textbook mentioned above.
-    # The confidence numbers are set to two decimal places to see if the
-    # same interval as in the textbook is returned for a few samples
-    # from the table
-    assert percentile_interval_places(n=5, p=0.5, c=0.93) == (1, 5)
-    assert percentile_interval_places(n=8, p=0.5, c=0.92) == (2, 7)
-    assert percentile_interval_places(n=11, p=0.5, c=0.93) == (3, 9)
-    assert percentile_interval_places(n=14, p=0.5, c=0.94) == (4, 11)
-    assert percentile_interval_places(n=17, p=0.5, c=0.95) == (5, 13)
-    assert percentile_interval_places(n=20, p=0.5, c=0.95) == (6, 15)
-
-    # The rest are regression tests
-    assert percentile_interval_places(n=60, p=0.8, c=0.5) == (45, 50)
-    assert percentile_interval_places(n=60, p=0.8, c=0.6) == (45, 51)
-    assert percentile_interval_places(n=60, p=0.8, c=0.7) == (44, 51)
-    assert percentile_interval_places(n=60, p=0.8, c=0.8) == (45, 53)
-    assert percentile_interval_places(n=60, p=0.8, c=0.9) == (42, 53)
-
-    assert percentile_interval_places(n=60, p=0.9, c=0.7) == (52, 57)
-    assert percentile_interval_places(n=60, p=0.9, c=0.8) == (52, 58)
-    assert percentile_interval_places(n=60, p=0.9, c=0.9) == (50, 58)
-    assert percentile_interval_places(n=60, p=0.9, c=0.95) == (50, 59)
-    assert percentile_interval_places(n=60, p=0.95, c=0.95) == (52, 60)
-
-    assert percentile_interval_places(n=60, p=0.8, c=0.2) == (46, 48)
-
-    assert percentile_interval_places(n=60, p=0.5, c=0.5) == (26, 32)
-    assert percentile_interval_places(n=60, p=0.5, c=0.7) == (25, 34)
-    assert percentile_interval_places(n=60, p=0.5, c=0.8) == (24, 35)
-    assert percentile_interval_places(n=60, p=0.5, c=0.9) == (24, 37)
-    assert percentile_interval_places(n=60, p=0.5, c=0.95) == (22, 38)
-    assert percentile_interval_places(n=60, p=0.5, c=0.99) == (20, 40)
-
-
-def test_tolerance_interval_indices() -> None:
-    assert tolerance_interval_places(n=60, t=0.8, c=0.5) == (5, 55)
-    assert tolerance_interval_places(n=60, t=0.8, c=0.7) == (4, 56)
-    assert tolerance_interval_places(n=60, t=0.8, c=0.8) == (5, 57)
-    assert tolerance_interval_places(n=60, t=0.8, c=0.9) == (4, 58)
-    assert tolerance_interval_places(n=60, t=0.85, c=0.85) == (4, 59)
-    assert tolerance_interval_places(n=60, t=0.9, c=0.9) == (3, 60)
-    assert tolerance_interval_places(n=120, t=0.95, c=0.95) == (2, 120)
-
-
 def test_assurance_in_quantile() -> None:
     assert assurance_in_percentile(14, 20) == pytest.approx(0.635, abs=0.001)
-
-
-def test_median_interval() -> None:
-    arr = range(10, 30)
-    assert confidence_interval_of_median(0.95, arr) == (15, 24)
-    arr_float = [k * 0.1 for k in range(10, 30)]
-    assert confidence_interval_of_median(0.95, arr_float) == (
-        pytest.approx(1.5, abs=0.01),
-        pytest.approx(2.4, abs=0.01),
-    )
-
-
-def test_quantile_interval() -> None:
-    arr = range(10, 30)
-    assert confidence_interval_of_percentile(0.75, 0.75, arr) == (22, 27)
-    assert confidence_interval_of_percentile(0.75, 0.9, arr) == (21, 28)
-    assert confidence_interval_of_percentile(0.5, 0.95, arr) == (15, 24)
-
-    arr_float = [k * 0.1 for k in range(10, 70)]
-    assert confidence_interval_of_percentile(0.8, 0.8, arr_float) == (
-        pytest.approx(5.4, abs=0.01),
-        pytest.approx(6.2, abs=0.01),
-    )
-
-
-def test_tolerance_interval() -> None:
-    arr = range(10, 30)
-    assert tolerance_interval(0.75, 0.75, arr) == (12, 29)
-    assert tolerance_interval(0.75, 0.9, arr) == (11, 29)
-    assert tolerance_interval(0.5, 0.95, arr) == (13, 28)
-    assert tolerance_interval(0.8, 0.8, arr) == (11, 29)
-    assert tolerance_interval(0.9, 0.9, arr) is None
-
-    arr_float = [k * 0.1 for k in range(10, 70)]
-    assert tolerance_interval(0.8, 0.8, arr_float) == (
-        pytest.approx(1.4, abs=0.01),
-        pytest.approx(6.6, abs=0.01),
-    )
-
-
-def test_assurance_interval() -> None:
-    assert assurance_in_interval(1, 15, 16) == pytest.approx(0.818, 0.001)
-    assert assurance_in_interval(1, 37, 38) == pytest.approx(0.901, 0.001)
-    assert assurance_in_interval(9, 28, 38) == pytest.approx(0.686, 0.001)
-    assert assurance_in_interval(7, 30, 38) == pytest.approx(0.731, 0.001)
-    assert assurance_in_interval(5, 32, 38) == pytest.approx(0.777, 0.001)
-    assert assurance_in_interval(3, 34, 38) == pytest.approx(0.824, 0.001)
-    assert assurance_in_interval(2, 36, 38) == pytest.approx(0.874, 0.001)
-    assert assurance_in_interval(1, 93, 94) == pytest.approx(0.951, 0.001)
